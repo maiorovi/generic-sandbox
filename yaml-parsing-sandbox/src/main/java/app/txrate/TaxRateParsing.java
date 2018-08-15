f83c9b25b28e7bc16d1f453528bcd6b95e817deb
yehor <maiorov.prog@gmail.com>
2018-08-15 23:06:17 +0300

    better to use map here

--- yaml-parsing-sandbox/src/main/java/app/txrate/TaxRateParsing.java
+++ yaml-parsing-sandbox/src/main/java/app/txrate/TaxRateParsing.java
@@ -1,6 +1,7 @@
 package app.txrate;
 
 import app.domain.Person;
+import app.domain.txrate.TaxRate;
 import app.domain.txrate.TaxRateItem;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -10,12 +11,13 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 
 public class TaxRateParsing {
     public static void main(String[] args) throws IOException {
         final Path path = Paths.get("/app/Projects/open-source/generic-sandbox/yaml-parsing-sandbox/src/main/resources/taxrate.yaml");
         final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        final List<TaxRateItem> rate = objectMapper.readValue(path.toFile(), List.class);
+        final List<Map<String, TaxRate>> rate = objectMapper.readValue(path.toFile(), List.class);
 
         System.out.println(rate);
     }
